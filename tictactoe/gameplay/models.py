a8d88f27ecc6a3450a912c153526821538ed5a3e
Saumil Jain <saumil-jain@users.noreply.github.com>
2018-01-19 11:59:19 +0530

    Add __str__ method to Game class for better representation of the records in the admin interface.

--- tictactoe/gameplay/models.py
+++ tictactoe/gameplay/models.py
@@ -10,6 +10,9 @@ class Game(models.Model):
     end_time = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=1, default='F')
 
+    def __str__(self):
+        return "{0} vs {1}".format(self.first_player, self.second_player)
+
 
 class Move(models.Model):
     x = models.IntegerField()
