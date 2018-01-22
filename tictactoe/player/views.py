c22eb47d72451a0bdfb43eef0f41648482d9967e
Saumil Jain <saumil-jain@users.noreply.github.com>
2018-01-22 20:55:50 +0530

    Add user's active games on player home
    Fetch all active games of the logged in user in the view function and pass
    it to the response as a list.
    Active games are those where the user is a player and it is the player's
    chance to move.
    
    Display the active games in home.html using django template for loop.

--- tictactoe/player/views.py
+++ tictactoe/player/views.py
@@ -4,4 +4,9 @@ from gameplay.models import Game
 
 
 def home(request):
-    return render(request, "player/home.html", {"ngames": Game.objects.count()})
+    games_first_player = Game.objects.filter(first_player=request.user, status='F')
+    games_second_player = Game.objects.filter(second_player=request.user, status='S')
+
+    all_my_games = list(games_first_player) + list(games_second_player)
+
+    return render(request, "player/home.html", {"ngames": Game.objects.count(), "games": all_my_games})
