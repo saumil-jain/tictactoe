c085cf74857dca54cf7a6c8546db350f52670aa2
Saumil Jain <saumil-jain@users.noreply.github.com>
2018-01-22 22:06:32 +0530

    Transfer the active game query from view to model
    Create a new class GameQuerySet which is a QuerySet.
    Create methods in this class to query for games for a user and active games.
    Use Q class to create OR conditions in the query.
    Set the GameQuerySet as Game class' manager, and use the methods to query.

--- tictactoe/player/views.py
+++ tictactoe/player/views.py
@@ -4,9 +4,7 @@ from gameplay.models import Game
 
 
 def home(request):
-    games_first_player = Game.objects.filter(first_player=request.user, status='F')
-    games_second_player = Game.objects.filter(second_player=request.user, status='S')
+    my_games = Game.objects.games_for_user(request.user)
+    active_games = my_games.active()
 
-    all_my_games = list(games_first_player) + list(games_second_player)
-
-    return render(request, "player/home.html", {"ngames": Game.objects.count(), "games": all_my_games})
+    return render(request, "player/home.html", {"ngames": Game.objects.count(), "games": active_games})
