2bdb9353644924356786e0a8f3a5ef2756573838
Saumil Jain <saumil-jain@users.noreply.github.com>
2018-02-10 13:17:42 +0530

    Add URL pattern in player urls.py for new invitation

--- tictactoe/player/urls.py
+++ tictactoe/player/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import home
+from .views import home, new_invitation
 
 
 urlpatterns = [
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'^login$',
         LoginView.as_view(template_name="player/login_form.html"),
         name="player_login"),
-    url(r'^logout$', LogoutView.as_view(), name="player_logout")
+    url(r'^logout$', LogoutView.as_view(), name="player_logout"),
+    url(r'^new_invitation$', new_invitation, name="player_new_invitation")
 ]
