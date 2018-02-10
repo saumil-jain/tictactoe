a37b92915a1d352f6bf230442502be7d47279e8c
Saumil Jain <saumil-jain@users.noreply.github.com>
2018-02-10 12:56:48 +0530

    Add InvitationForm ModelForm class to generate an Invitation form

---
+++ tictactoe/player/forms.py
@@ -0,0 +1,9 @@
+from django.forms import ModelForm
+
+from .models import Invitation
+
+
+class InvitationForm(ModelForm):
+    class Meta:
+        model = Invitation
+        fields = ["to_user", "message"]
