5414e0f74edc280ab4841a5f129a39b2f7e367d3
Saumil Jain <saumil-jain@users.noreply.github.com>
2018-02-18 22:05:12 +0530

    Add help text and verbose name for to_user and message Invitation model fields
    This will be used in the form display.
    Also allow message field to be blank in form validations.
    Add corresponding migrations file.

--- tictactoe/player/models.py
+++ tictactoe/player/models.py
@@ -5,6 +5,17 @@ from django.contrib.auth.models import User
 
 class Invitation(models.Model):
     from_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="invitations_sent")
-    to_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="invitations_received")
-    message = models.CharField(max_length=300)
+    to_user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="invitations_received",
+        verbose_name="User to invite",
+        help_text="Please select a user you want to play a game with."
+    )
+    message = models.CharField(
+        max_length=300,
+        blank=True,
+        verbose_name="Optional Message",
+        help_text="Add an optional friendly invite message!"
+    )
     timestamp = models.DateTimeField(auto_now_add=True)
