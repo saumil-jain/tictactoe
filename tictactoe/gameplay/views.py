64cbf3d95455fe9eae6073c6227a97f51bf1ac8b
Saumil Jain <saumil-jain@users.noreply.github.com>
2018-02-25 19:26:59 +0530

    Enhance game detail view to add the Move form to the template context
    The form will only be added to the context when it is the
    currently logged in user's move.

--- tictactoe/gameplay/views.py
+++ tictactoe/gameplay/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 from .models import Game
+from .forms import MoveForm
 
 
 @login_required
 def game_detail(request, id):
     game = get_object_or_404(Game, pk=id)
-    return render(request, "gameplay/game_detail.html", {"game": game})
+    context = {"game": game}
+    # only if it is the user's move, then show the form
+    if game.is_users_move(request.user):
+        context["form"] = MoveForm()
+    return render(request, "gameplay/game_detail.html", context)
