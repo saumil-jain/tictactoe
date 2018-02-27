2c4f02b2277b540d1d0aeadd32d746e9b68a4b88
Saumil Jain <saumil-jain@users.noreply.github.com>
2018-02-27 11:58:12 +0530

    Add url pattern for making a move
    It takes as a parameter the id of the game.

--- tictactoe/gameplay/urls.py
+++ tictactoe/gameplay/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from .views import game_detail
+from .views import game_detail, make_move
 
 
 urlpatterns = [
-    url(r"^game_details/(?P<id>\d+)/$", game_detail, name="gameplay_detail")
+    url(r"^game_details/(?P<id>\d+)/$", game_detail, name="gameplay_detail"),
+    url(r"^make_move/(?P<id>\d+)", make_move, name="gameplay_make_move")
 ]
